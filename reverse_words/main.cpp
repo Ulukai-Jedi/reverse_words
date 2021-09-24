a7d88cfd3b4bc229516e5254db38e7c248a6d654
Gergely Brunda <gergelybrunda@gmail.com>
2021-09-24 02:58:30 +0200

    Add project files.

---
+++ reverse_words/main.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void reverseStr(string& str)
+{
+    int n = str.length();
+
+    for (int i = 0; i < n / 2; i++)
+        swap(str[i], str[n - i - 1]);
+}
+
+
+int main()
+{
+    string s;
+    getline(cin, s);
+    reverseStr(s);
+    cout << s;
+    return 0;
+}
+
+/*class Solution {
+public:
+    string reverseWords(string s) {
+        reverse(s.begin(), s.end());
+        reverseWordss(s);
+        return getString(s);
+    }
+    string getString(string s) {
+        int i = 0;
+        int j = s.size() - 1;
+        while (s[i] == ' ' && i < s.size()) i++;
+        while (j >= 0 && s[j] == ' ') j--;
+        string ret = "";
+        for (; i <= j; i++) {
+            if (ret.size() && ret.back() == ' ' && s[i] == ' ')continue;
+            ret += s[i];
+        }
+        return ret;
+    }
+    void reverseWordss(string& s) {
+        int j = 0;
+        for (int i = 0; i < s.size(); i = j) {
+            if (s[i] == ' ') {
+                j = i + 1;
+            }
+            else {
+                while (j + 1 < s.size() && s[j + 1] != ' ') j++;
+                int x = i;
+                int y = j;
+                while (x < y) {
+                    swap(s[x], s[y]);
+                    x++;
+                    y--;
+                }
+                j++;
+            }
+        }
+    }
+};
+int main() {
+    Solution ob;
+    string s;
+    getline(cin, s);
+    ob.reverseWordss(s);
+    cout << s;
+}*/
